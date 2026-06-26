6fda273835e1613f8d9276148fa64e1f337e57e5
vanlinh.vu <vanlinh.vu@gmail.com>
2026-06-26 22:00:58 +0700

    build: tang phien ban len v4.38 (code 59)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sdv.lichnoti"
         minSdk = 26
         targetSdk = 35
-        versionCode = 58
-        versionName = "4.37"
+        versionCode = 59
+        versionName = "4.38"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
